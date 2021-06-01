6fc01e58f48412eb8396e48daa0c24a4c7595321
amgrsmsport <56305816+amgrsmsport@users.noreply.github.com>
2021-06-01 16:16:49 +0530

    dependency added router

--- src/App.js
+++ src/App.js
@@ -8,19 +8,5 @@ import { Member2 } from "./views/Member2";
 import { Member3 } from "./views/Member3";
 
 // TEAM LEADER
-function App() {
-  return (
-    <div className="container-fluid">
-      <div className="row" style={{ height: "100vh" }}>
-        <div className="col-12 col-md-6 bg-dark text-light d-flex align-items-center justify-content-center">
-          <Member1></Member1>
-        </div>
-        <div className="col-12 col-md-6 bg-secondary text-light d-flex align-items-center justify-content-center">
-          <Member2></Member2>
-        </div>
-      </div>
-    </div>
-  );
-}
 
 export default App;
