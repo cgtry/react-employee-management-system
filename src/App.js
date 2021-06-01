704c4128248dab103aff7b93be567b1017495929
amgrsmsport <56305816+amgrsmsport@users.noreply.github.com>
2021-06-01 15:45:25 +0530

    members in separate folder

--- src/App.js
+++ src/App.js
@@ -3,162 +3,21 @@ import "./App.css";
 
 import { useState } from "react";
 import { Button, ButtonGroup, Col, Row } from "react-bootstrap";
+import { Member1 } from "./views/Member1";
+import { Member2 } from "./views/Member2";
+import { Member3 } from "./views/Member3";
 
+// TEAM LEADER
 function App() {
   return (
     <div>
-      <Member1></Member1>
+      <Member1 />
 
       <div className="mb-5"></div>
       <Member2 />
 
       <div className="mb-5"></div>
       <Member3 />
-
-      <div className="mb-5"></div>
-      <Member4></Member4>
-    </div>
-  );
-}
-
-function Member1() {
-  return (
-    <div>
-      <h1>Member 1 - Registeration</h1>
-      <div>
-        <div className="mb-1">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter Username"
-            className="form-control"
-          />
-        </div>
-
-        <div className="mb-1">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter Password"
-            className="form-control"
-          />
-        </div>
-
-        <div className="mb-1">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter Email"
-            className="form-control"
-          />
-        </div>
-
-        <div className="mb-1">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter Mobile"
-            className="form-control"
-          />
-        </div>
-
-        <div className="mb-1">
-          <input
-            type="button"
-            value="Register"
-            className="btn btn-primary w-100"
-          />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function Member2() {
-  return (
-    <div>
-      <h1>Member 2 - Login Module</h1>
-      <div className="mb-1">
-        <input
-          type="text"
-          placeholder="Enter Username"
-          className="form-control"
-        />
-      </div>
-
-      <div className="mb-1">
-        <input
-          type="text"
-          placeholder="Enter Password"
-          className="form-control"
-        />
-      </div>
-
-      <div>
-        <Button className="w-100">Login Here</Button>
-      </div>
-    </div>
-  );
-}
-
-function Member3() {
-  return (
-    <div>
-      <h1>Forgot Password Module</h1>
-
-      <div className="mb-1">
-        <input
-          type="text"
-          placeholder="Enter Username"
-          className="form-control"
-        />
-      </div>
-
-      <div className="mb-1">
-        <input
-          type="text"
-          placeholder="Old Password"
-          className="form-control"
-        />
-      </div>
-
-      <div className="mb-1">
-        <input
-          type="text"
-          placeholder="New Password"
-          className="form-control"
-        />
-      </div>
-
-      <div className="mb-1">
-        <input
-          type="text"
-          placeholder="Re Enter New Password"
-          className="form-control"
-        />
-      </div>
-
-      <div>
-        <input
-          type="button"
-          value="Reset Password"
-          className="btn btn-primary w-100"
-        />
-      </div>
-    </div>
-  );
-}
-
-function Member4() {
-  return (
-    <div>
-      <div>
-        <h2>Member 4 module</h2>{" "}
-      </div>
     </div>
   );
 }
